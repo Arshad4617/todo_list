c9d2121a0980f6de0cfcc68fb19ef2f35fd1757f
arshad46 <arshad4617@gmail.com>
2021-06-30 18:40:07 +0530

    front end and todo create funtion created

---
+++ app/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.utils import timezone
+from . models import Todo
+from django.http import HttpResponseRedirect
+# Create your views here.
+def home(request):
+    todo_items = Todo.objects.all().order_by("-added_date")
+    context = {
+        "todo_items": todo_items
+    }
+    return render(request, 'app/index.html', context)
+
+@csrf_exempt
+def add_todo(request):
+    content = request.POST['content']
+    current_date = timezone.now()
+    created_obj = Todo.objects.create(added_date=current_date, text=content)
+
+    return HttpResponseRedirect('/')
